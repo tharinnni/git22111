b8eb2fc9cdf41439ae425067b6fc8c8710f803e1
tharinnni <138591570+tharinnni@users.noreply.github.com>
2024-10-22 22:44:34 +0530

    Update FrameworkException.java
    add

--- src/main/java/com/anhtester/exceptions/FrameworkException.java
+++ src/main/java/com/anhtester/exceptions/FrameworkException.java
@@ -19,7 +19,36 @@ public class FrameworkException extends RuntimeException {
     public FrameworkException(String message, Throwable cause) {
         super(message, cause);
     }
-    
+    @SuppressWarnings("serial")
+public class FrameworkException extends RuntimeException {
+
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    public FrameworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    @SuppressWarnings("serial")
+public class FrameworkException extends RuntimeException {
+
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    public FrameworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    @SuppressWarnings("serial")
+public class FrameworkException extends RuntimeException {
+
+    public FrameworkException(String message) {
+        super(message);
+    }
+
+    public FrameworkException(String message, Throwable cause) {
+        super(message, cause);
+    }
     @SuppressWarnings("serial")
     public class FrameworkException extends RuntimeException {
 
