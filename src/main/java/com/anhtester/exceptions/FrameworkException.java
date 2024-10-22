81a20e93a2143f4d941db479dc0033c320486516
tharinnni <tharinimtcs@gmail.com>
2024-10-22 22:38:56 +0530

    src/main/java/com/anhtester/exceptions/FrameworkException.java added

--- src/main/java/com/anhtester/exceptions/FrameworkException.java
+++ src/main/java/com/anhtester/exceptions/FrameworkException.java
@@ -19,4 +19,15 @@ public class FrameworkException extends RuntimeException {
     public FrameworkException(String message, Throwable cause) {
         super(message, cause);
     }
+    
+    @SuppressWarnings("serial")
+    public class FrameworkException extends RuntimeException {
+
+        public FrameworkException(String message) {
+            super(message);
+        }
+
+        public FrameworkException(String message, Throwable cause) {
+            super(message, cause);
+        }
 }
