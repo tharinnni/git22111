e051de8fbcb3d54e0ef8a5a743218442f241a54e
tharinnni <138591570+tharinnni@users.noreply.github.com>
2024-10-22 22:52:52 +0530

    Update FrameworkException.java
    ss

--- src/main/java/com/anhtester/exceptions/FrameworkException.java
+++ src/main/java/com/anhtester/exceptions/FrameworkException.java
@@ -1,16 +1,1 @@
-/*
- * Copyright (c) 2022.
- * Automation Framework Selenium - Anh Tester
- */
 
-package com.anhtester.exceptions;
-
-/**
- * FrameworkException extends RuntimeException - because I want to terminate the program when the Exception
- * comes
- */
-@SuppressWarnings("serial")
-public class FrameworkException extends RuntimeException {
-
-
-}
