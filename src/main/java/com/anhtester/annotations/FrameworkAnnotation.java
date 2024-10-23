a83e7ccd6e15afa6936dbdc94b3a7350fddfcb96
tharinnni <tharinimtcs@gmail.com>
2024-10-23 12:04:34 +0530

    aded

--- src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
+++ src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
@@ -7,3 +7,13 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+
+
+import com.anhtester.enums.AuthorType;
+import com.anhtester.enums.CategoryType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
