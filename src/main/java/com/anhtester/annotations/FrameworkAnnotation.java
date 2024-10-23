f0ac1dd6dbbdd91ad5cec491f31b84d39d1be456
tharinnni <tharinimtcs@gmail.com>
2024-10-23 12:01:15 +0530

    added

--- src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
+++ src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
@@ -7,13 +7,3 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-<<<<<<< HEAD
-new branchiem
-stash apply
-=======
-
-
->>>>>>> 939a77ea7c7f3419e3b98594cc975996372ba2a5
-
-}
