939a77ea7c7f3419e3b98594cc975996372ba2a5
tharinnni <tharinimtcs@gmail.com>
2024-10-23 11:25:18 +0530

    added

--- src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
+++ src/main/java/com/anhtester/annotations/FrameworkAnnotation.java
@@ -8,16 +8,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-//This is an Custom Annotation
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface FrameworkAnnotation {
 
-    // This is not a method
-    public AuthorType[] author();
-
-    // public String[] category();
-    public CategoryType[] category();
 
 
 }
